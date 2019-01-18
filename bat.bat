7bc478f897d3d48d623d01ce094e8ea0bdbf87ce
askudnov <skudnoff.a@yandex.ru>
2019-01-18 14:24:22 +0300

    fix idea

--- bat.bat
+++ bat.bat
@@ -1,4 +1,5 @@
-javac -d bin -sourcepath src -cp resources\libs\spring-context-5.1.4.RELEASE.jar;resources\libs\spring-core-5.1.4.RELEASE.jar;resources\libs\spring-beans-5.1.4.RELEASE.jar src\com\StartMain.java src\com\lib\*.java 
+mkdir bin
+javac -d bin -sourcepath src -cp resources\libs\spring-context-5.1.4.RELEASE.jar;resources\libs\spring-core-5.1.4.RELEASE.jar;resources\libs\spring-beans-5.1.4.RELEASE.jar src\com\lib\StartMain.java src\com\lib\*.java 
 chcp 65001 
-java -classpath bin;resources\libs\commons-logging-1.2.jar;resources\libs\spring-context-5.1.4.RELEASE.jar;resources\libs\spring-core-5.1.4.RELEASE.jar;resources\libs\spring-beans-5.1.4.RELEASE.jar;resources\libs\spring-expression-5.1.4.RELEASE.jar;resources\libs\spring-aop-5.1.4.RELEASE.jar com.lib.StartMain
+java -classpath bin;resources\libs\commons-logging-1.2.jar;resources\libs\spring-context-5.1.4.RELEASE.jar;resources\libs\spring-core-5.1.4.RELEASE.jar;resources\libs\spring-beans-5.1.4.RELEASE.jar;resources\libs\spring-expression-5.1.4.RELEASE.jar;resources\libs\spring-aop-5.1.4.RELEASE.jar lib.StartMain
 pause
